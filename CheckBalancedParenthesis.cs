b7b691d554fc646ed09d33276d0699fc6863cb93
DevscopeTutorial <149447867+DevscopeTutorial@users.noreply.github.com>
2023-10-31 12:59:21 +1100

    Check Balanced Parenthesis

---
+++ CheckBalancedParenthesis.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+
+namespace CheckBaalancedParenteses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String expr = "({[]})";
+            if (isExpressionBalanced(expr))
+            {
+                Console.WriteLine(expr + " => Balanced" );
+            }
+            else 
+            {
+                Console.WriteLine(expr + " => Not Balanced");
+            }
+        }
+
+        private static bool isExpressionBalanced(string expr)
+        {
+            Stack stack = new Stack();
+            foreach (char current in expr)
+            {
+                if (stack.Count > 0 && isSameKind((char)stack.Peek(), current))
+                {
+                    //Pop top element
+                    stack.Pop();
+                }
+                else 
+                {
+                    //Push current bracket
+                    stack.Push(current); 
+                }
+            }
+
+            
+            if(stack.Count>0)
+                return false;
+            else
+                return true;
+        }
+
+        private static bool isSameKind(char first, char second)
+        {
+            if (first == '[' && second == ']' || first == '{' && second == '}' || first == '(' && second == ')')
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
