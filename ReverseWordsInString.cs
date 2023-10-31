93e1aaac2c68d51c8c38b412230d548d6014b3d1
DevscopeTutorial <149447867+DevscopeTutorial@users.noreply.github.com>
2023-10-31 12:58:27 +1100

    Reverse Words In String

---
+++ ReverseWordsInString.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+namespace ReverseWordsInString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputStr = "This is a sample Text";
+            string[] strArray = inputStr.Split(' ');
+            foreach (string str in strArray)
+            {
+                ReverseWord(str);
+            }
+        }
+
+        private static void ReverseWord(string str)
+        {
+            Stack stack = new Stack();
+            foreach (char ch in str) 
+            {
+                stack.Push(ch);
+            }
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+            Console.Write(' ');
+        }
+    }
+}
